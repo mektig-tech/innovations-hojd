d1394d842f910ca87ab8e2a5b004bb9d71bfbfbc
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2025-06-09 14:45:05 +0200

    feat: Update App component layout and add Wingler section

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,69 @@
 import "./index.css";
 
 import logo from "./logo.svg";
+import logoWingler from "./wingler_symbol.svg";
 
 export function App() {
 	return (
-		<div className="max-w-7xl mx-auto p-4 relative z-10">
-			<div className="flex-col sm:justify-center items-center flex gap-26 max-w-[1000px] animate-fadeIn delay-500">
-				<img src={logo} className=" w-full  animate-spin-slow" alt="Bun logo" />
-				<div className="pl-2 sm:pl-4 flex flex-col gap-4 border-l-6 border-black/40">
-					<h1 className="text-4xl sm:text-5xl font-black animate-fadeIn">
-						Verksamhet
-					</h1>
-					<p className="text:lg sm:text-2xl text-gray-700 animate-fadeIn">
-						Innovationsutvecklingsbolag som utvecklar idéer i syfte att skapa
-						nya lösningar med ett genomslag som ger en värdeskapande positiv
-						förändring.
-					</p>
+		<>
+			<section className="max-w-7xl mx-auto p-4 relative z-10 min-h-screen grid place-items-center">
+				<div className="flex-col sm:justify-center items-center flex gap-26 max-w-[1000px] animate-fadeIn delay-500">
+					<img
+						src={logo}
+						className=" w-full  animate-spin-slow"
+						alt="Bun logo"
+					/>
+					<div className="pl-2 sm:pl-4 flex flex-col gap-4 border-l-6 border-black/40">
+						<h1 className="text-4xl sm:text-5xl font-black animate-fadeIn">
+							Verksamhet
+						</h1>
+						<p className="text:lg sm:text-2xl text-gray-700 animate-fadeIn">
+							Innovationsutvecklingsbolag som utvecklar idéer i syfte att skapa
+							nya lösningar med ett genomslag som ger en värdeskapande positiv
+							förändring.
+						</p>
+					</div>
+					<div className="text-center">
+						<p className="text-lg mb-4">Innovationshöjd Sverige AB</p>
+						<p>Besöksadress: Kungsgatan 43, 632 17 Eskilstuna</p>
+						<a
+							href="mailto:info@innovationshojd.se"
+							className="text-blue-500 hover:underline"
+						>
+							info@innovationshojd.se
+						</a>
+					</div>
 				</div>
-				<div className="text-center">
-					<p className="text-lg mb-4">Innovationshöjd Sverige AB</p>
-					<p>Besöksadress: Kungsgatan 43, 632 17 Eskilstuna</p>
-					<a
-						href="mailto:lennart.kisswani@innovationshojd.se"
-						className="text-blue-500 hover:underline"
-					>
-						lennart.kisswani@innovationshojd.se
-					</a>
+			</section>
+			<section className="text-white mx-auto p-4 relative z-10 min-h-screen grid place-items-center bg-gradient-to-br from-wing-base to-wing-base-neutral">
+				<div className="flex-col sm:justify-center items-center flex gap-26 max-w-[1000px] animate-fadeIn delay-500">
+					<img
+						src={logoWingler}
+						className="w-full h-90 animate-spin-slow"
+						alt="Bun logo"
+					/>
+					<div className="pl-2 sm:pl-4 flex flex-col gap-4 border-l-6 border-white/40">
+						<h1 className="text-4xl sm:text-5xl font-black animate-fadeIn">
+							Wingler®
+						</h1>
+						<p className="text:lg sm:text-2xl text-gray-300 animate-fadeIn">
+							Innovatinshöjds första mobilapplication, Wingler®. Wingler är en
+							smart samåknings app som kalkulerar alla resekostnader
+						</p>
+					</div>
+					<div className="text-center">
+						<p className="text-lg mb-4">Innovationshöjd Sverige AB</p>
+						<p>Besöksadress: Kungsgatan 43, 632 17 Eskilstuna</p>
+						<a
+							href="mailto:lennart.kisswani@innovationshojd.se"
+							className="text-blue-500 hover:underline"
+						>
+							lennart.kisswani@innovationshojd.se
+						</a>
+					</div>
 				</div>
-			</div>
-		</div>
+			</section>
+		</>
 	);
 }
 
