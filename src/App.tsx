1d661af497a0b6c1a6fd75698818cdec96e9fa00
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2025-06-11 13:50:10 +0200

    feat: Revise Wingler section description and update link text in App component

--- src/App.tsx
+++ src/App.tsx
@@ -47,30 +47,27 @@ export function App() {
 							Wingler®
 						</h1>
 						<p className="text:lg sm:text-2xl text-gray-300 animate-fadeIn">
-							Innovationshöjd presenterar stolt sin första mobilapplikation –
-							<span className="text-wing-accent"> Wingler®</span>
+							Innovationshöjd presenterar stolt sin första app{" "}
+							<span>Wingler®</span>
 							<br />
+							<span>Wingler®</span> är en innovativ samåkningsapp som gör det
+							enkelt att dela resor och kostnader. Appen räknar automatiskt och
+							rättvist ut vad varje person ska betala, perfekt för allt från
+							vardagspendling till helgresor och spontana bilfärder.
 							<br />
-							<span className="text-wing-accent"> Wingler®</span> är en
-							innovativ samåkningsapp som gör det enkelt att resa smartare,
-							tillsammans. Appen beräknar automatiskt och rättvist alla
-							resekostnader, vilket gör det smidigt att dela på kostnaderna –
-							oavsett om det gäller pendling, weekendresor eller spontana
-							bilfärder. Med lansering på både iOS och Android förenklar
-							<span className="text-wing-accent"> Wingler®</span> vardagen för
-							både förare och passagerare – och bidrar samtidigt till ett mer
-							hållbart resande.
+							Tillgänglig för både iOS och Android, <span>Wingler®</span> gör
+							det smidigt för förare och passagerare att resa tillsammans.
+							Resultatet? En enklare vardag och ett mer hållbart resande.
 						</p>
 					</div>
 					<div className="text-center">
-						<p className="text-lg mb-4">Wingler®</p>
 						<a
 							href="https://www.wingler.app"
 							target="_blank"
 							className="text-blue-500 hover:underline"
 							rel="noreferrer"
 						>
-							www.wingler.app
+							Till innovations höjd hemsida Wingler
 						</a>
 					</div>
 				</div>
